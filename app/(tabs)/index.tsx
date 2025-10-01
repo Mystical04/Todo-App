dd14983aef7a2fcbfa443e455c5368795d89a64d
Wafiq <wafiqamrin2@gmail.com>
2025-10-01 20:02:45 +0800

    Added colors scheme and make toggle dark mode

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import useTheme from "@/hooks/useTheme";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const {toggleDarkMode} = useTheme();
   return (
     <View style={styles.container}>
       <Text style={styles.content}>Edit app/index.tsx to edit this screen.</Text>
       <Text>Hi</Text>
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text>Toggle Mode</Text>
+      </TouchableOpacity>
     </View>
   );
 }
