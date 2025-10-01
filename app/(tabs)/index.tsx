6a2e505496fc66543d0770bfd022c895e7f50201
Wafiq <wafiqamrin2@gmail.com>
2025-10-02 01:15:52 +0800

    Testing convex CRUD

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,6 +3,8 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const {toggleDarkMode} = useTheme();
+
+  
   return (
     <View style={styles.container}>
       <Text style={styles.content}>Edit app/index.tsx to edit this screen.</Text>
